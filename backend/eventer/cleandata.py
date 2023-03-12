6ac61a0bcae457b6123b193ce8fb16c8cb4ebc36
Jakub Kościelny <kuba1095@gmail.com>
2023-03-12 23:17:15 +0100

    cleaner and correct solution

--- backend/eventer/cleandata.py
+++ backend/eventer/cleandata.py
@@ -3,15 +3,29 @@ import os
 import datetime
 from time import sleep
 
+ip_rest_api = os.environ.get('REST_API_IP')
+url = "http://"+ip_rest_api +"/api/cleanoldlogs/"
+headers = {"Content-Type": "application/json; charset=utf-8"}
+  
 
+sleep(15)
+print(url)
 print("Program started...")
 while True:
 
     temp_time = datetime.datetime.now()
-    if(temp_time.hour==23 and temp_time.minute==59):
-        requests.get(
-            os.environ.get('REST_API_IP')+"/api/cleanoldlogs/"
-        )
+    if(temp_time.hour==19 and temp_time.minute==22):
+        error = ""
+        try:
+            r1 = requests.get(
+                url+"1",
+                headers=headers
+            )
+        except Exception as e:
+            error = e
         temp_time_str = temp_time.strftime("%m/%d/%Y, %H:%M:%S")
-        print(f"Done {temp_time_str}")
-    sleep(59)
+        if error=="":
+            print(f"Done {temp_time_str} {r1.json()}")
+        else:
+            print(f"Error {temp_time_str} {error}")
+    sleep(60)
